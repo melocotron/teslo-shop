5de2c8c51cfd331cdb0cf1668ff6066e3cde18a2
Melocotron <uriarcia@gmail.com>
2023-12-18 17:59:16 -0600

    feat: 151 seed 1ra parte

---
+++ src/seed/seed.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Delete } from '@nestjs/common';
+import { SeedService } from './seed.service';
+
+@Controller('seed')
+export class SeedController {
+  constructor(private readonly seedService: SeedService) {}
+
+  @Get()
+  executeSeed() {
+    return this.seedService.runSeed();
+  }
+
+  private async insertNewProducts() {
+    //this.
+  }
+}
