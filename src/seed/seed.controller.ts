9c0808a5c6a2ef0515b97e2c398fec728b127380
melocotron <uriarcia@gmail.com>
2024-01-03 14:45:36 -0600

    feat: 187 comment unused imports

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { SeedService } from './seed.service';
-import { Auth } from '../auth/decorators';
-import { ValidRoles } from '../auth/interfaces';
+// import { Auth } from '../auth/decorators';
+// import { ValidRoles } from '../auth/interfaces';
 
 @Controller('seed')
 export class SeedController {
